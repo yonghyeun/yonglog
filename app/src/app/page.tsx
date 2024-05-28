795188079862f6c47f6775135a5f702842919266
unknown <ttddcc119@naver.com>
2024-05-28 11:36:33 +0900

    Home 문구 수정

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>Vercel 테스트</h1>
+      <h1>Feature Brach First Test</h1>
     </main>
   );
 }
