9a24afda97b4846d815deb1bf0d54592b9870a9f
yonghyeun <ttddcc119@naver.com>
2024-05-31 12:32:44 +0900

    SEO , footer 추가

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -10,7 +10,7 @@ const Page = () => {
       </div>
       <CategoryList />
       {/* 게시글 리스트와 SideBar를 위치시키기 위함 */}
-      <section className='w-full lg:w-[120%] flex gap-5 h-[150vh]'>
+      <section className='w-full lg:w-[120%] flex gap-5 h-[100vh]'>
         {/* TODO page 생성 후 border 지우기 */}
         <section className='border bg-white w-full lg :w-8/12'>
           {/* TODO 게시글 리스트들 넣기 */}
